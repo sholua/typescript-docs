fecdbc51db009bb0cb6ecf0d0b5e89859d5ec578
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-08-14 21:04:09 +0300

    Interface

--- build/interface.js
+++ build/interface.js
@@ -1,51 +1,27 @@
-// /////////////// Readonly properties
-// interface User {
-//     readonly name: string;
-//     age?: number;
-// }
-// const user: User = {
-//     name: "Alex",
-// }
-// user.name = 'test';
-// /////////////// Interface extension
-// interface User {
-//     name: string;
-//     age: number;
-//     [propName: string]: any;
-// }
-// const user: User = {
-//     name: 'test',
-//     age: 25,
-//     address: 'test',
-// }
-// //////////////// Class Interface
-// interface User {
-//     name: string;
-//     age: number;
-// }
-// interface Pass {
-//     getPass(): string;
-// }
-// class Alex implements User, Pass {
-//     name = 'Alex';
-//     age = 25;
-//     getPass = function () {
-//         return 'test';
-//     }
-//     additionalProp = 'test';
-// }
-// //////////////// Interface ixtends
-// interface User {
-//     name: string;
-//     age: number;
-// }
-// interface Admin extends User {
-//     getPass(): string;
-// }
-// class Owner implements Admin {
-//     name = 'Alex';
-//     age = 35;
-//     getPass() {
-//         return `${this.name} is ${this.age} years old`;
-//     }
-// }
+const user7 = {
+    name: "Alex",
+};
+const user8 = {
+    name: "test",
+    age: 25,
+    address: "test",
+};
+class Alex {
+    constructor() {
+        this.name = "Alex";
+        this.age = 25;
+        this.getPass = function () {
+            return "test";
+        };
+        this.additionalProp = "test";
+    }
+}
+class Owner {
+    constructor() {
+        this.name = "Alex";
+        this.age = 35;
+    }
+    getPass() {
+        return `${this.name} is ${this.age} years old`;
+    }
+}
--- src/interface.ts
+++ src/interface.ts
@@ -1,61 +1,59 @@
-// /////////////// Readonly properties
-// interface User {
-//     readonly name: string;
-//     age?: number;
-// }
+/////////////// Readonly properties
+interface User7 {
+  readonly name: string;
+  age?: number;
+}
 
-// const user: User = {
-//     name: "Alex",
-// }
+const user7: User7 = {
+  name: "Alex",
+};
 
-// user.name = 'test';
+/////////////// Interface extension
+interface User8 {
+  name: string;
+  age: number;
+  [propName: string]: any;
+}
 
-// /////////////// Interface extension
-// interface User {
-//     name: string;
-//     age: number;
-//     [propName: string]: any;
-// }
+const user8: User8 = {
+  name: "test",
+  age: 25,
+  address: "test",
+};
 
-// const user: User = {
-//     name: 'test',
-//     age: 25,
-//     address: 'test',
-// }
+//////////////// Class Interface
+interface User9 {
+  name: string;
+  age: number;
+}
 
-// //////////////// Class Interface
-// interface User {
-//     name: string;
-//     age: number;
-// }
+interface Pass {
+  getPass(): string;
+}
 
-// interface Pass {
-//     getPass(): string;
-// }
+class Alex implements User9, Pass {
+  name = "Alex";
+  age = 25;
+  getPass = function () {
+    return "test";
+  };
+  additionalProp = "test";
+}
 
-// class Alex implements User, Pass {
-//     name = 'Alex';
-//     age = 25;
-//     getPass = function () {
-//         return 'test';
-//     }
-//     additionalProp = 'test';
-// }
+//////////////// Interface ixtends
+interface User10 {
+  name: string;
+  age: number;
+}
 
-// //////////////// Interface ixtends
-// interface User {
-//     name: string;
-//     age: number;
-// }
+interface Admin extends User10 {
+  getPass(): string;
+}
 
-// interface Admin extends User {
-//     getPass(): string;
-// }
-
-// class Owner implements Admin {
-//     name = 'Alex';
-//     age = 35;
-//     getPass() {
-//         return `${this.name} is ${this.age} years old`;
-//     }
-// }
+class Owner implements Admin {
+  name = "Alex";
+  age = 35;
+  getPass() {
+    return `${this.name} is ${this.age} years old`;
+  }
+}
