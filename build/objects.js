aed4eb1416f88fe57f7fd2ebe7ec498e17c3dc96
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-08-14 19:08:12 +0300

    Objects

---
+++ build/objects.js
@@ -0,0 +1,15 @@
+// Define object type
+let user = {
+    name: "Alex",
+    age: 30,
+};
+let user2 = {
+    name: "Alex",
+    age: 30,
+    nickName: "shol",
+};
+let user3 = {
+    name: "Alex",
+    age: 20,
+    nickName: "test",
+};
